b2193323fd673bcc25f2333d200bb5b25dea4c45
Bart <kluczynskib@gmail.com>
2016-07-21 16:47:24 +0200

    Challenge 21
    As Player 1,
    So I can win a game of Battle,
    I want to attack Player 2, and I want to get a confirmation

--- app.rb
+++ app.rb
@@ -5,13 +5,10 @@ class Battle < Sinatra::Base
   enable :sessions
 
   get '/' do
-    #'Hello Battle!'
     erb :index
   end
 
   post '/names' do
-    # @player_1_name = params[:player_1_name]
-    # @player_2_name = params[:player_2_name]
     session[:player1] = params[:player1]
     session[:player2] = params[:player2]
     redirect '/play'
@@ -21,7 +18,14 @@ class Battle < Sinatra::Base
     @player_1 = session[:player1]
     @player_2 = session[:player2]
     erb :play
-    end
+  end
+
+  get '/attack' do
+    @player_1 = session[:player1]
+    @player_2 = session[:player2]
+    erb :attack
+  end
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
---
+++ spec/features/attacking_player2_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+  RSpec.feature "Battle" do
+  scenario 'player 1 attacks player 2' do
+    sign_in_and_play
+    click_button('Attack')
+    expect(page).to have_content('John attacks Jane')
+  end
+end
