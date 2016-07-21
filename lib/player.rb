e03958d7f2abd1c0f541877dcf356c033d72e629
Luke Cartledge <luke@lukecartledge.com>
2016-07-21 13:01:22 +0100

    Ch24

--- lib/player.rb
+++ lib/player.rb
@@ -1,12 +1,17 @@
 class Player
 
 DEFAULT_HIT_POINTS = 60
+DEFAULT_ATTACK = 10
 
   attr_reader :name, :hit_points
 
   def initialize(name, hit_points = DEFAULT_HIT_POINTS)
     @name = name
     @hit_points = hit_points
+  end
+
+  def attack(player)
+    @hit_points -= DEFAULT_ATTACK
   end
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,6 +3,7 @@ require 'player'
 describe Player do
 
   subject(:titus) { described_class.new("Titus") }
+  subject(:karol) { described_class.new("Karol") }
 
   describe "#name" do
     it 'returns the name of a player' do
@@ -13,6 +14,12 @@ describe Player do
   describe "#hit_points" do
     it "starts with the default amount of hit points" do
       expect(titus.hit_points).to eq 60
+    end
+  end
+
+  describe "#attack" do
+    it "reduces the players hit_points by 10" do
+      expect{ karol.attack(titus) }.to change{ titus.hit_points }.by (-10)
     end
   end
 end
