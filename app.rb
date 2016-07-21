1cbc9d14200e496b5d94d40ebd8e6e2446d78371
alwinsolanky <alwinsolanky@gmail.com>
2016-07-21 16:53:50 +0100

    extraction initial part

--- app.rb
+++ app.rb
@@ -1,28 +1,29 @@
 require 'sinatra/base'
+require_relative './lib/player'
 
 class Battle < Sinatra::Base
 
-  enable :sessions
+  # enable :sessions
 
   get '/' do
     erb :index
   end
 
   post '/names' do
-    session[:player1] = params[:player1]
-    session[:player2] = params[:player2]
+    $player1 = Player.new(params[:player1])
+    $player2 = Player.new(params[:player2])
     redirect '/play'
   end
 
   get '/play' do
-    @player_1 = session[:player1]
-    @player_2 = session[:player2]
+    @player_1 = $player1.name
+    @player_2 = $player2.name
     erb :play
   end
 
   get '/attack' do
-    @player_1 = session[:player1]
-    @player_2 = session[:player2]
+    @player_1 = $player1.name
+    @player_2 = $player2.name
     erb :attack
   end
 
