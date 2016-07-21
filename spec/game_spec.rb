891b3acf2180075fe2c1faea13d7d844cc0fae23
Luke Cartledge <luke@lukecartledge.com>
2016-07-21 15:31:42 +0100

    Ch26 - adds skinny controllers - lots of refactoring

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,11 +4,24 @@ describe Game do
 
   let(:titus) { double :player }
   let(:karol) { double :player }
-  subject(:game) { described_class.new }
+  subject(:game) { described_class.new(titus, karol) }
   describe "#attack" do
     it "reduces the players hit_points by 10" do
       expect(titus).to receive(:receive_damage)
       game.attack(titus)
     end
   end
+
+  describe "#player_1" do
+    it "returns player 1" do
+      expect(game.player_1).to eq titus
+    end
+  end
+
+  describe "#player_2" do
+    it "returns player 2" do
+      expect(game.player_2).to eq karol
+    end
+  end
+
 end
