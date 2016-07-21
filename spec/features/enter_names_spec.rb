95b5ebbe9179b2cfa38b66c3a49ce03afb812a38
alwinsolanky <alwinsolanky@gmail.com>
2016-07-21 12:48:29 +0100

    Step 19 post redirect

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -6,8 +6,8 @@ RSpec.feature "Battle", :type => :feature do
     # fill_in 'names', with: 'John'
     # click_button 'Submit'
     # expect(page).to have_text('John')
-    fill_in :player_1_name, with: 'John'
-    fill_in :player_2_name, with: 'Jane'
+    fill_in :player1, with: 'John'
+    fill_in :player2, with: 'Jane'
     click_button 'Submit'
     expect(page).to have_content 'John V. Jane'
 
