d283e7ff2c060ac80c497a2f7e7d06f016e1c2b2
alwinsolanky <alwinsolanky@gmail.com>
2016-07-21 14:58:51 +0100

    Test Helpers added

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -2,14 +2,7 @@ require 'spec_helper'
 
 RSpec.feature "Battle", :type => :feature do
   scenario "Enter player names" do
-    visit '/'
-    # fill_in 'names', with: 'John'
-    # click_button 'Submit'
-    # expect(page).to have_text('John')
-    fill_in :player1, with: 'John'
-    fill_in :player2, with: 'Jane'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'John V. Jane'
-
   end
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -2,11 +2,7 @@ require 'spec_helper'
 
 RSpec.feature "Battle", :type => :feature do
   scenario "returns the hit points of player 2" do
-    visit '/'
-    fill_in :player1, with: 'John'
-    fill_in :player2, with: 'Jane'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content ("Jane: 60HP")
-
   end
 end
