85cbe362610bce7660fe1df009fde72c836c2777
Bart <kluczynskib@gmail.com>
2016-07-22 13:37:59 +0200

    Challenge 29
    As a Player,
    So I can Lose a game of Battle,
    I want to see a 'Lose' message if I reach 0HP first

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -7,4 +7,14 @@ feature "View hit points" do
     sign_in_and_play
     expect(page).to have_content("Karol: 60HP" )
   end
+  scenario "it prints out the message that you've lost in case the HP is 0 " do
+    sign_in_and_play
+    11.times do
+      click_button 'Attack!'
+      click_button 'Switch Player'
+    end
+    expect(page).to have_content("You've lost!")
+  end
+
+
 end
